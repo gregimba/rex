804d2af68adb1b2c81bcb0537b06b660730e8668
Grant Regimbal <regimbal.grant@gmail.com>
2018-04-24 16:27:06 -0500

    hardcode fix db

--- database/index.js
+++ database/index.js
@@ -1,26 +1,21 @@
 const Sequelize = require('sequelize');
 
-// change host to process.env.DATABASE_URL
-
-const host = `postgres://cweicgpabdtjhg:f29d128c28622c3861240d4e18e3fe0f02dff49a11d49dc02f962137973bc22f@ec2-54-243-213-188.compute-1.amazonaws.com:5432/d1dqjsl382dfk3`;
-//  || {
-//   database: "rex",
-//   username: "Mike",
-//   password: null,
-//   dialect: "postgres"
-// };
-
-const sequelize = new Sequelize(host, {
+const sequelize = new Sequelize('rex', 'grant', 'null', {
+  host: 'localhost',
   dialect: 'postgres',
-  dialectOptions: {
-    ssl: true
+
+  pool: {
+    max: 5,
+    min: 0,
+    acquire: 30000,
+    idle: 10000
   }
 });
 
 sequelize
   .authenticate()
-  .then(() => console.log("connection made"))
-  .catch((err) => console.log(`cannot connect: ${err}`));
+  .then(() => console.log('connection made'))
+  .catch(err => console.log(`cannot connect: ${err}`));
 
 exports.promiseQuery = query =>
   sequelize.query(query, { type: sequelize.QueryTypes.SELECT });
